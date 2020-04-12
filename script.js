0f5ef4524ef4987f3ceffad335c3b98765aedb83
aviki <toldi.victoria@gmail.com>
2020-04-12 17:57:17 +0200

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+import { gsap } from "gsap";
+import { TweenLite } from "gsap/all";
+import { TweenMax } from "gsap/all";
+import { Power1 } from "gsap/all";
+import { Bounce } from "gsap/all";
+import { Elastic } from "gsap/all";
+import { _createElement } from "gsap/CSSPlugin";
+
+("use strict");
+
+window.addEventListener("DOMContentLoaded", init);
